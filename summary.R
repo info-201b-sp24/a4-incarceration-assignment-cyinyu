ab7be9bb1ce8427ec33c360e06bbaad3979b8e09
cyinyu <cyinyu@uw.edu>
2024-06-03 14:47:05 -0700

    commit message

--- summary.R
+++ summary.R
@@ -9,42 +9,33 @@ summary_info$num_county <- prison_wa %>%
   pull(num)
 summary_info$max_jail_rate <- prison_wa %>%
   filter(total_jail_pop_rate == max(total_jail_pop_rate, na.rm = TRUE)) %>%
-  pull(total_jail_pop_rate)
+  select(total_jail_pop_rate)
 summary_info$max_jail_rate_black <- prison_wa %>%
-  filter(total_jail_pop_rate == max(black_jail_pop_rate, na.rm = TRUE)) %>%
-  pull(total_jail_pop_rate)
+  filter(black_jail_pop_rate == max(black_jail_pop_rate, na.rm = TRUE)) %>%
+  select(black_jail_pop_rate)
 summary_info$max_jail_rate_black_county <- prison_wa %>%
-  filter(total_jail_pop_rate == max(black_jail_pop_rate, na.rm = TRUE)) %>%
-  pull(county_name)
+  filter(black_jail_pop_rate == max(black_jail_pop_rate, na.rm = TRUE)) %>%
+  select(county_name)
 summary_info$diff_max_race_jail_rate <- prison_wa %>%
   filter(year == max(year)) %>%
   summarize(max_diff = max(black_jail_pop_rate, na.rm = TRUE) -
               max(white_jail_pop_rate, na.rm = TRUE)) %>%
-  pull(max_diff)
+  select(max_diff)
 summary_info$recent_avg_jail_rate_white <- prison_wa %>%
   filter(year == max(year)) %>%
   summarize(avg_rate = mean(white_jail_pop_rate, na.rm = TRUE)) %>%
-  pull(avg_rate)
+  select(avg_rate)
 summary_info$recent_avg_jail_rate_black <- prison_wa %>%
   filter(year == max(year)) %>%
   summarize(avg_rate = mean(black_jail_pop_rate, na.rm = TRUE)) %>%
-  pull(avg_rate)
+  select(avg_rate)
 summary_info$change_avg_black_jail_rate <- prison_wa %>%
   group_by(year) %>%
   summarize(avg_rate = mean(black_jail_pop_rate, na.rm = TRUE)) %>%
   filter(year == max(year) | year == min(year)) %>%
   summarize(change = diff(avg_rate)) %>%
-  pull(change)
+  select(change)
 
 
 
-
-
-
-ex1 <- prison_wa %>%
-  group_by(year) %>%
-  summarize(avg_rate = mean(total_jail_pop_rate)) %>%
-  filter(year == max(year) | year == min(year)) %>%
-  summarize(change = diff(avg_rate))
-View(ex1)
 View(summary_info)
